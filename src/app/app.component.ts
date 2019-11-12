b97105819be0485d4942d8d5e7b6d5565d331d69
GuillermoMafla <cjativa12@hotmail.com>
2019-11-11 22:07:44 -0500

    Primer Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  title = 'hospital';
+
+
+
+  ngOnInit(){
+    $(document).ready(function () {
+      $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+      });
+  });
+  }
+}
+
+
+
